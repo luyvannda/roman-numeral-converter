9fa6cffe156e3935bc5465cb3f6ed26b490c18c5
luyvannda <philips.joe329@gmail.com>
2023-08-16 15:38:00 +0700

    Run into an infinite loop, will have to be caution with the code

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
+const romanEquivalent = {
+  1000: "M",
+  900: "CM",
+  500: "D",
+  400: "CD",
+  100: "C",
+  90: "XC",
+  50: "L",
+  40: "XL",
+  10: "X",
+  9: "IX",
+  5: "V",
+  4: "IV",
+  1: "I"
+};
+
 function convertToRoman(num) {
-  const romanEquivalent = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I"
-  };
-  return num;
+
+  let output = "";
+
+  for (let key in romanEquivalent) {
+    const value = +key
+
+    while (value <= num) {
+      output += romanEquivalent[key];
+      console.log(output)
+    }
+  }
+
+
+  return;
 }
 
-convertToRoman(2);
+let result = convertToRoman(2);
+console.log(result);
